74629d683691f240094d344db335d70668314278
abdulrojak12345678 <rojaka140507@gmail.com>
2024-05-16 16:53:30 +0700

    update

---
+++ konfigurasi-firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBcym1i4oAyM2rFmBU_Ipa0vcC7Pdz0dws",
+  authDomain: "insan-cemerlang-2e18f.firebaseapp.com",
+  projectId: "insan-cemerlang-2e18f",
+  storageBucket: "insan-cemerlang-2e18f.appspot.com",
+  messagingSenderId: "1096016420480",
+  appId: "1:1096016420480:web:87611389fc765e7ddbd065",
+  measurementId: "G-DW23S2DXCR"
+};
